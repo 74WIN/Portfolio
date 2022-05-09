43b38970b697d3bec33ebe811c8feb72f4479c54
74WINXD <ccw.la@outlook.com>
2022-05-09 13:04:17 +0200

    Ik heb socials en de collapse aangepast

---
+++ index.js
@@ -0,0 +1,64 @@
+window.addEventListener('load', init)
+
+let projects = [
+    {
+        name: "Sushi King Reservering systeem",
+        image: {
+                screenshot: "", 
+                banner: ""
+        }, 
+        description: "",
+        url: {
+            demo: "",
+            app: ""
+        },
+    },
+    {
+        name: "The Greedy Spy",
+        image: {
+                screenshot: "", 
+                banner: ""
+        }, 
+        description: "",
+        url: {
+            demo: "",
+            app: ""
+        },
+    },
+    {
+        name: "Weapon API",
+        image: {
+                screenshot: "", 
+                banner: ""
+        }, 
+        description: "",
+        url: {
+            demo: "",
+            app: ""
+        },
+    },
+    {
+        name: "",
+        image: {
+                screenshot: "", 
+                banner: ""
+        }, 
+        description: "",
+        url: {
+            demo: "",
+            app: ""
+        },
+    },
+    {
+        name: "",
+        image: {
+                screenshot: "", 
+                banner: ""
+        }, 
+        description: "",
+        url: {
+            demo: "",
+            app: ""
+        },
+    },
+]
