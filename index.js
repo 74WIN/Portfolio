8990f3e99da6b200e707724ec64ec2a1ee45d2be
74WINXD <ccw.la@outlook.com>
2022-05-14 15:43:34 +0200

    added modal

--- index.js
+++ index.js
@@ -1,64 +1,71 @@
 window.addEventListener('load', init)
 
-let projects = [
-    {
-        name: "Sushi King Reservering systeem",
-        image: {
-                screenshot: "", 
-                banner: ""
-        }, 
-        description: "",
-        url: {
-            demo: "",
-            app: ""
-        },
-    },
-    {
-        name: "The Greedy Spy",
-        image: {
-                screenshot: "", 
-                banner: ""
-        }, 
-        description: "",
-        url: {
-            demo: "",
-            app: ""
-        },
-    },
-    {
-        name: "Weapon API",
-        image: {
-                screenshot: "", 
-                banner: ""
-        }, 
-        description: "",
-        url: {
-            demo: "",
-            app: ""
-        },
-    },
-    {
-        name: "",
-        image: {
-                screenshot: "", 
-                banner: ""
-        }, 
-        description: "",
-        url: {
-            demo: "",
-            app: ""
-        },
-    },
-    {
-        name: "",
-        image: {
-                screenshot: "", 
-                banner: ""
-        }, 
-        description: "",
-        url: {
-            demo: "",
-            app: ""
-        },
-    },
-]
+let myModal = document.getElementById('myModal')
+let myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
+
+// let projects = [
+//     {
+//         name: "Sushi King Reservering systeem",
+//         image: {
+//                 screenshot: "", 
+//                 banner: ""
+//         }, 
+//         description: "",
+//         url: {
+//             demo: "",
+//             app: ""
+//         },
+//     },
+//     {
+//         name: "The Greedy Spy",
+//         image: {
+//                 screenshot: "", 
+//                 banner: ""
+//         }, 
+//         description: "",
+//         url: {
+//             demo: "",
+//             app: ""
+//         },
+//     },
+//     {
+//         name: "Weapon API",
+//         image: {
+//                 screenshot: "", 
+//                 banner: ""
+//         }, 
+//         description: "",
+//         url: {
+//             demo: "",
+//             app: ""
+//         },
+//     },
+//     {
+//         name: "",
+//         image: {
+//                 screenshot: "", 
+//                 banner: ""
+//         }, 
+//         description: "",
+//         url: {
+//             demo: "",
+//             app: ""
+//         },
+//     },
+//     {
+//         name: "",
+//         image: {
+//                 screenshot: "", 
+//                 banner: ""
+//         }, 
+//         description: "",
+//         url: {
+//             demo: "",
+//             app: ""
+//         },
+//     },
+// ]
